e74fcb552c3fb20769d0b47ef90242f7407cb983
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-06-07 01:32:16 +0300

    add cookie message

--- script.js
+++ script.js
@@ -28,3 +28,39 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(".header");
+const allSection = document.querySelectorAll(".section");
+console.log(allSection);
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+
+console.log(document.getElementsByClassName("btn"));
+
+// creating and inserting elements
+// .insertAdjacentHTML;
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent =
+//   "We use cookie for improved functionality and analytics. ";
+message.innerHTML =
+  "We use cookie for improved functionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>";
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document.querySelector(".btn--close-cookie").addEventListener("click", () => {
+  message.remove();
+});
