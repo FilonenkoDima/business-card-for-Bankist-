56a943350c8d8a9c440b1e8e78cd19968924618e
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-06-19 01:15:48 +0300

    removed commented code

--- script.js
+++ script.js
@@ -43,29 +43,12 @@ btnScrollTo.addEventListener("click", (e) => {
 });
 
 // Page navigation
-
-// document.querySelectorAll(".nav__link").forEach((el) => {
-//   el.addEventListener("click", function (e) {
-//     e.preventDefault();
-
-//     const id = this.getAttribute("href");
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({
-//       behavior: "smooth",
-//     });
-//   });
-// });
-
-// 1. Add event listenet to common parent element
-// 2. Datermine what element originated the event
-
 document.querySelector(".nav__links").addEventListener("click", function (e) {
   e.preventDefault();
 
   // Matching atrategy
   if (e.target.classList.contains("nav__link")) {
     const id = e.target.getAttribute("href");
-    console.log(id);
     document.querySelector(id).scrollIntoView({
       behavior: "smooth",
     });
@@ -111,38 +94,11 @@ nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
 
 // Sticky navigation
-// const initialCoords = section1.getBoundingClientRect();
-// console.log(initialCoords);
-
-// window.addEventListener("scroll", function () {
-//   console.log(window.scrollY);
-
-//   if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
-//   else nav.classList.remove("sticky");
-// });
-
-// Sticky navigation: Intersection Observer API
-
-// const obsCallback = function (entries, observer) {
-//   entries.forEach((entry) => {
-//     console.log(entry);
-//   });
-// };
-
-// const obsOptions = {
-//   root: null,
-//   threshold: [0, 0.2],
-// };
-
-// const observer = new IntersectionObserver(obsCallback, obsOptions);
-// observer.observe(section1);
-
 const header = document.querySelector(".header");
 const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  // console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
@@ -160,7 +116,6 @@ const allSections = document.querySelectorAll(".section");
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  // console.log(entry);
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove("section--hidden");
@@ -174,7 +129,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  // section.classList.add("section--hidden");
+  section.classList.add("section--hidden");
 });
 
 // Lazy loading images
@@ -272,7 +227,6 @@ const slider = function () {
   btnLeft.addEventListener("click", prevSlide);
 
   document.addEventListener("keydown", function (e) {
-    console.log(e);
     if (e.key === "ArrowLeft") prevSlide();
     e.key === "ArrowRight" && nextSlide();
   });
@@ -287,179 +241,10 @@ const slider = function () {
 };
 
 slider();
-////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-// Selecting elements
-// console.log(document.documentElement);
-// console.log(document.head);
-// console.log(document.body);
 
-// const header = document.querySelector(".header");
-// const allSection = document.querySelectorAll(".section");
-// console.log(allSection);
-
-// document.getElementById("section--1");
-// const allButtons = document.getElementsByTagName("button");
-// console.log(allButtons);
-
-// console.log(document.getElementsByClassName("btn"));
-
-// creating and inserting elements
-// .insertAdjacentHTML;
-// const message = document.createElement("div");
-// message.classList.add("cookie-message");
-// message.textContent =
-//   "We use cookie for improved functionality and analytics. ";
-// message.innerHTML =
-//   "We use cookie for improved functionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>";
-
-// header.prepend(message);
-// header.append(message);
-// header.append(message.cloneNode(true));
-
-// header.before(message);
-// header.after(message);
-
-// Delete elements
-// document.querySelector(".btn--close-cookie").addEventListener("click", () => {
-//   message.remove();
-// });
-
-// Styles
-// message.style.backgroundColor = "#37383d";
-// message.style.width = "104%";
-
-// console.log(message.style.color);
-// console.log(message.style.backgroundColor);
-
-// console.log(getComputedStyle(message).color);
-// console.log(getComputedStyle(message).height);
-
-// message.style.height =
-//   Number.parseFloat(getComputedStyle(message).height, 10) + 40 + "px";
-/*
-document.documentElement.style.setProperty("--color-primary", "orangered");
-
-// Attributes
-const logo = document.querySelector(".nav__logo");
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
-
-logo.alt = "Beatiful minimalist logo";
-
-// Non-standart
-console.log(logo.designer);
-console.log(logo.getAttribute("designer"));
-logo.setAttribute("company", "Bankist");
-
-console.log(logo.src);
-console.log(logo.getAttribute("src"));
-
-const link = document.querySelector(".nav__link--btn");
-console.log(link.href);
-
-console.log(link.getAttribute("href"));
-
-// Data attributes
-console.log(logo.dataset.versionNumber);
-
-// Classes
-logo.classList.add("c", "j");
-logo.classList.remove("c", "j");
-logo.classList.toggle("c");
-logo.classList.contains("c"); // not includes
-
-// Don't use
-logo.className = "Dima";
-*/
-
-/*
-const h1 = document.querySelector("h1");
-
-const alertH1 = (e) => {
-  alert("addEventListener: Great! You are reading the heading :D");
-};
-
-h1.addEventListener("mouseenter", alertH1);
-
-setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
-
-// h1.onmouseenter = (e) => {
-//   alert("onmouseenter: Great! You are reading the heading :D");
-// };
-*/
-
-/*
-
-// rgb(255, 255, 255)
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
-const randomColor = () =>
-  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-console.log(randomColor());
-
-document.querySelector(".nav__link").addEventListener("click", function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log("LINK", e.target, e.currentTarget);
-  console.log(e.currentTarget === this);
-
-  // Stop propagation
-  // e.stopPropagation();
-});
-
-document.querySelector(".nav__links").addEventListener("click", function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log("CONTAINER", e.target, e.currentTarget);
-  console.log(e.currentTarget === this);
-});
-
-document.querySelector(".nav").addEventListener("click", function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log("NAV", e.target, e.currentTarget);
-  console.log(e.currentTarget === this);
-});
-*/
-
-// const h1 = document.querySelector("h1");
-
-// // Going downwards: child
-// console.log(h1.querySelectorAll(".highlight"));
-// console.log(h1.childNodes);
-// console.log(h1.children);
-// h1.firstElementChild.style.color = "white";
-// h1.lastElementChild.style.color = "orangered";
-
-// // Going upwards: parents
-// console.log(h1.parentElement);
-
-// h1.closest(".header").style.background = "var(--gradient-secondary)";
-
-// h1.closest("h1").style.background = "var(--gradient-primary)";
-
-// // Going sideways: siblings
-// console.log(h1.previousElementSibling);
-// console.log(h1.nextElementSibling);
-
-// console.log(h1.previousSibling);
-// console.log(h1.nextSibling);
-
-// console.log(h1.parentElement.children);
-// [...h1.parentElement.children].forEach(function (el) {
-//   if (el !== h1) el.style.transform = "scale(0.5)";
-// });
-
-// document.addEventListener("DOMContentLoaded", function (e) {
-//   console.log("HTML parsed and DOM tree built!");
-// });
-
-// window.addEventListener("load", function (e) {
-//   console.log("Page fully loaded", e);
-// });
+// confirm of close tab
 
 window.addEventListener("beforeunload", function (e) {
   e.preventDefault();
-  console.log(e);
   e.returnValue = "";
 });
